c8d2e5419c471647379ef631d64436e44dc35615
JDByte01 <30425866+JDByte01@users.noreply.github.com>
2017-08-12 21:35:50 -0600

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-#C:\Users\JOSED\Documents\Visual Studio 2017\Projects\[EDD]Practica1\servidor
-
-	# =================
-	# ||Configuracion||
-	# =================
-
-from app import app # de la carpeta app importar 
-					#la variable app que contiene a Flask
-app.run(debug = True)
