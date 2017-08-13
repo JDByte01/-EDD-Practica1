d6e9f760b2bfce9f3c89460266baeddb711947d7
JDByte01 <30425866+JDByte01@users.noreply.github.com>
2017-08-12 21:36:59 -0600

    Delete __init__.py

--- app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__) 	#Variable app que contiene a Flask
-from app import views 	#Paquete [app] carpeta donde esta el archivo __init__.py
-						#Se importa views.py
